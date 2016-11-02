bc65ce36e7deeb2a68b0d4273a53d2a9546de05b
Stefan Walther <Stefan.Walther@qlik.com>
2016-11-02 12:47:14 +0100

    build: remove group

--- test/integration/test.sh
+++ test/integration/test.sh
@@ -2,18 +2,18 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 cleanup () {
-  docker-compose -p ci kill
-  docker-compose -p ci rm -f
+  docker-compose kill
+  docker-compose rm -f
 }
 trap 'cleanup ; printf "${RED}Tests Failed For Unexpected Reasons${NC}\n"' HUP INT QUIT PIPE TERM
 #docker-compose -p ci build && docker-compose -p ci up -d
-docker-compose  -p ci up -d --build
+docker-compose  up -d --build
 if [ $? -ne 0 ] ; then
   printf "${RED}Docker Compose Failed${NC}\n"
   exit -1
 fi
-TEST_EXIT_CODE=`docker wait ci_flyway_rest_integration`
-docker logs ci_flyway_rest_integration
+TEST_EXIT_CODE=`docker wait flyway_rest_integration`
+docker logs flyway_rest_integration
 if [ -z ${TEST_EXIT_CODE+x} ] || [ "$TEST_EXIT_CODE" -ne 0 ] ; then
   printf "${RED}Tests Failed${NC} - Exit Code: $TEST_EXIT_CODE\n"
 else
