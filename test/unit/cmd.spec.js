764f0c9766dd02b43e34a41bf8114bb927036896
Stefan Walther <Stefan.Walther@qlik.com>
2016-11-10 21:52:22 -0500

    test: Fix unit test, considering -q

--- test/unit/cmd.spec.js
+++ test/unit/cmd.spec.js
@@ -9,21 +9,21 @@ describe( 'cli:buildCommand', () => {
 
   it( 'Returns a command if flyway args are passed.', () => {
     expect( cmd.buildCommand.bind( null, { url: 'foo' } ) ).not.to.throw( Error );
-    expect( cmd.buildCommand( { url: 'foo' } ) ).to.be.equal( 'flyway -url=foo info' );
+    expect( cmd.buildCommand( { url: 'foo' } ) ).to.be.equal( 'flyway -q -url=foo info' );
   } );
 
   it( 'cli:buildCommand:command has to one of ["clean", "info", "validate", "baseline", "repair", "migrate"]', () => {
-    expect( cmd.buildCommand( { url: 'foo' }, 'clean' ) ).to.be.equal( 'flyway -url=foo clean' );
-    expect( cmd.buildCommand( { url: 'foo' }, 'info' ) ).to.be.equal( 'flyway -url=foo info' );
-    expect( cmd.buildCommand( { url: 'foo' }, 'validate' ) ).to.be.equal( 'flyway -url=foo validate' );
-    expect( cmd.buildCommand( { url: 'foo' }, 'baseline' ) ).to.be.equal( 'flyway -url=foo baseline' );
-    expect( cmd.buildCommand( { url: 'foo' }, 'repair' ) ).to.be.equal( 'flyway -url=foo repair' );
-    expect( cmd.buildCommand( { url: 'foo' }, 'migrate' ) ).to.be.equal( 'flyway -url=foo migrate' );
+    expect( cmd.buildCommand( { url: 'foo' }, 'clean' ) ).to.be.equal( 'flyway -q -url=foo clean' );
+    expect( cmd.buildCommand( { url: 'foo' }, 'info' ) ).to.be.equal( 'flyway -q -url=foo info' );
+    expect( cmd.buildCommand( { url: 'foo' }, 'validate' ) ).to.be.equal( 'flyway -q -url=foo validate' );
+    expect( cmd.buildCommand( { url: 'foo' }, 'baseline' ) ).to.be.equal( 'flyway -q -url=foo baseline' );
+    expect( cmd.buildCommand( { url: 'foo' }, 'repair' ) ).to.be.equal( 'flyway -q -url=foo repair' );
+    expect( cmd.buildCommand( { url: 'foo' }, 'migrate' ) ).to.be.equal( 'flyway -q -url=foo migrate' );
     expect( cmd.buildCommand.bind( null, { url: 'foo' }, 'bar' ) ).to.throw( 'Invalid Flyway action.' );
   } );
 
   it( 'cli:buildCommand:command defaults to "info"', () => {
-    expect( cmd.buildCommand( { url: 'foo' } ) ).to.be.equal( 'flyway -url=foo info' );
+    expect( cmd.buildCommand( { url: 'foo' } ) ).to.be.equal( 'flyway -q -url=foo info' );
   } );
 
 } );
