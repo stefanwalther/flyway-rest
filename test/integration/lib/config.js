8f74a2a029ac31864de1cdc3ca10f0264a210b9e
Stefan Walther <Stefan.Walther@qlik.com>
2016-11-19 22:01:23 +0100

    test: more configs

--- test/integration/lib/config.js
+++ test/integration/lib/config.js
@@ -1,4 +1,7 @@
 
-export const FLYWAY_REST_PORT = process.env.FLYWAY_REST_PORT || 9001;
 export const FLYWAY_REST_HOST = process.env.FLYWAY_REST_HOST || 'localhost';
+export const FLYWAY_REST_PORT = process.env.FLYWAY_REST_PORT || 9001;
 export const FLYWAY_REST_URL = `http://${FLYWAY_REST_HOST}:${FLYWAY_REST_PORT}`;
+export const FLYWAY_REST_DB_HOST = process.env.FLYWAY_REST_DB_HOST || 'flyway_rest_db';
+export const FLYWAY_REST_DB_PORT = process.env.FLYWAY_REST_DB_PORT || 5432;
+export const FLYWAY_REST_DB_URL = ``;
